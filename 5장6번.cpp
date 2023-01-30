55063e322641f13f9857e1f6e41cd3dc613801f3
NaEunMin <123241668+NaEunMin@users.noreply.github.com>
2023-01-30 23:37:31 +0900

    Add files via upload
    control reaches end of non-void function 오류가 뜨긴하는데 결과는 나온다.
    return 값이 없어서 그렇다고 찾아보니 나왔다. return을 해줬는데 무언가가 안맞나보다.
    왜 그런지 찾아봐야할듯

---
+++ 5장6번.cpp
@@ -0,0 +1,36 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+char& find(char a[], char c, bool& success){
+    for(int i=0;i<strlen(a);i++){
+        if(a[i]==c){
+            success = true;
+            return a[i];
+        }
+        else
+        success == false;
+    }
+}
+
+int main()
+{
+    char s[] = "Mike";
+    bool b = false;
+    char &loc = find(s, 'M', b);
+    if(b==false){
+        cout << "M을 발견할 수 없다" << endl;
+        return 0;
+    }
+    loc = 'm';
+    cout << s << endl;
+}
